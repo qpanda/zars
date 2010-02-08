9cca31271b1c877309e8962bd91a648984a85f0e
Erich Liebmann <7557807+qpanda@users.noreply.github.com>
2010-02-08 14:19:11 +0000

    InvoiceDao does not support remove method

--- src/main/webapp/net/soomsam/zirmegghuette/zars/persistence/dao/jpa/JpaInvoiceDao.java
+++ src/main/webapp/net/soomsam/zirmegghuette/zars/persistence/dao/jpa/JpaInvoiceDao.java
@@ -1,6 +1,7 @@
 package net.soomsam.zirmegghuette.zars.persistence.dao.jpa;
 
 import net.soomsam.zirmegghuette.zars.persistence.dao.InvoiceDao;
+import net.soomsam.zirmegghuette.zars.persistence.dao.OperationNotSupportedException;
 import net.soomsam.zirmegghuette.zars.persistence.entity.Invoice;
 
 import org.springframework.stereotype.Repository;
@@ -10,5 +11,10 @@ public class JpaInvoiceDao extends JpaEntityDao<Invoice> implements InvoiceDao {
 	@Override
 	protected Class<Invoice> determineEntityClass() {
 		return Invoice.class;
+	}
+
+	@Override
+	public void remove(Invoice entity) {
+		throw new OperationNotSupportedException("[" + JpaInvoiceDao.class.getSimpleName() + "] does not support operation 'remove'");
 	}
 }
