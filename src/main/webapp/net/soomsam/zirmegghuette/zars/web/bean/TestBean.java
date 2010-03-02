50064689852f27d74e06905cbcb177c8d8d7aa2d
Erich Liebmann <7557807+qpanda@users.noreply.github.com>
2010-03-02 10:25:54 +0000

    added javax.validation framwork to get validation with annotations to work

--- src/main/webapp/net/soomsam/zirmegghuette/zars/web/bean/TestBean.java
+++ src/main/webapp/net/soomsam/zirmegghuette/zars/web/bean/TestBean.java
@@ -5,6 +5,8 @@ import java.io.Serializable;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import net.soomsam.zirmegghuette.zars.service.UserService;
 
@@ -15,6 +17,18 @@ public class TestBean implements Serializable {
 	private transient UserService userService;
 
 	private final String test = "abc";
+
+	@NotNull
+	@Size(min = 2, message = "{customerNameError}")
+	private String lastName;
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(final String lastName) {
+		this.lastName = lastName;
+	}
 
 	public String getTest() {
 		return test;
