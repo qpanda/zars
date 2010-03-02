d4a6455c890c9fb3d86651b3b217777f1f53b649
Erich Liebmann <7557807+qpanda@users.noreply.github.com>
2010-03-02 05:50:21 +0000

    added i18n support testing @Inject

--- src/main/webapp/net/soomsam/zirmegghuette/zars/web/bean/TestBean.java
+++ src/main/webapp/net/soomsam/zirmegghuette/zars/web/bean/TestBean.java
@@ -3,15 +3,29 @@ package net.soomsam.zirmegghuette.zars.web.bean;
 import java.io.Serializable;
 
 import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+
+import net.soomsam.zirmegghuette.zars.service.UserService;
 
 @Named
 @SessionScoped
 public class TestBean implements Serializable {
+	@Inject
+	private transient UserService userService;
+
+	private final String test = "abc";
+
+	public String getTest() {
+		return test;
+	}
+
 	public String signup() {
 		if (Math.random() < 0.2) {
+			System.out.println("$$$" + userService);
 			return ("accepted");
 		} else {
+			System.out.println("$$$" + userService);
 			return ("rejected");
 		}
 	}
