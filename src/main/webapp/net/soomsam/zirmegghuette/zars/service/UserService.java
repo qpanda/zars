4fcb7d430c9b8e520f4c6681528bc256e40de818
Erich Liebmann <7557807+qpanda@users.noreply.github.com>
2010-06-02 11:40:28 +0000

    added enum message bundle

--- src/main/webapp/net/soomsam/zirmegghuette/zars/service/UserService.java
+++ src/main/webapp/net/soomsam/zirmegghuette/zars/service/UserService.java
@@ -5,9 +5,9 @@ import java.util.List;
 import net.soomsam.zirmegghuette.zars.service.bean.RoleBean;
 
 public interface UserService {
-	public static final String ROLE_USER = "user";
-	public static final String ROLE_ADMIN = "admin";
-	public static final String ROLE_ACCOUNTANT = "accountant";
+	public static final String ROLE_USER = "ROLE_USER";
+	public static final String ROLE_ADMIN = "ROLE_ADMIN";
+	public static final String ROLE_ACCOUNTANT = "ROLE_ACCOUNTANT";
 
 	public void createAllRoles();
 
