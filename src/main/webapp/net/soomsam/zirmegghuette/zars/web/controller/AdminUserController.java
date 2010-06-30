6514b6c3ae13ad72a68f7121773f7ed4409a6e85
Erich Liebmann <7557807+qpanda@users.noreply.github.com>
2010-06-30 10:25:54 +0000

    admin user use case

--- src/main/webapp/net/soomsam/zirmegghuette/zars/web/controller/AdminUserController.java
+++ src/main/webapp/net/soomsam/zirmegghuette/zars/web/controller/AdminUserController.java
@@ -3,6 +3,7 @@ package net.soomsam.zirmegghuette.zars.web.controller;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.faces.event.ActionEvent;
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -10,6 +11,7 @@ import net.soomsam.zirmegghuette.zars.service.UserService;
 import net.soomsam.zirmegghuette.zars.service.bean.UserBean;
 
 import org.apache.log4j.Logger;
+import org.primefaces.component.commandlink.CommandLink;
 import org.springframework.context.annotation.Scope;
 
 @Named
@@ -17,13 +19,49 @@ import org.springframework.context.annotation.Scope;
 public class AdminUserController implements Serializable {
 	private final static Logger logger = Logger.getLogger(AdminUserController.class);
 
+	private final String commandLinkSelectedUserIdAttributeName = "selectedUserId";
+
+	private Long selectedUserId;
+
 	@Inject
 	private transient UserService userService;
 
+	public String getCommandLinkSelectedUserIdAttributeName() {
+		return commandLinkSelectedUserIdAttributeName;
+	}
+
+	public Long getSelectedUserId() {
+		return selectedUserId;
+	}
+
+	public void setSelectedUserId(final Long selectedUserId) {
+		this.selectedUserId = selectedUserId;
+	}
+
 	public List<UserBean> getAllUsers() {
-		return userService.finaAllUsers();
+		return userService.findAllUsers();
+	}
+
+	public void setSelectedUserId(final ActionEvent commandLinkActionEvent) {
+		if ((null != commandLinkActionEvent) && (commandLinkActionEvent.getComponent() instanceof CommandLink)) {
+			final CommandLink commandLink = (CommandLink) commandLinkActionEvent.getComponent();
+			final Long commandLinkParameterValue = (Long) commandLink.getAttributes().get(commandLinkSelectedUserIdAttributeName);
+			setSelectedUserId(commandLinkParameterValue);
+		}
 	}
 
 	public void doStuff() {
+	}
+
+	public String disableUser() {
+		userService.disableUser(selectedUserId);
+		logger.debug("disabled user with id [" + selectedUserId + "]");
+		return null;
+	}
+
+	public String enableUser() {
+		userService.enableUser(selectedUserId);
+		logger.debug("enabled user with id [" + selectedUserId + "]");
+		return null;
 	}
 }
