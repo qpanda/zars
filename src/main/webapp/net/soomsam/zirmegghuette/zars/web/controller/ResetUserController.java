0cdebedb80b954ba0938e783bc9663d0cb497793
Erich Liebmann <7557807+qpanda@users.noreply.github.com>
2010-07-07 09:51:06 +0000

    reset user use case

---
+++ src/main/webapp/net/soomsam/zirmegghuette/zars/web/controller/ResetUserController.java
@@ -0,0 +1,104 @@
+package net.soomsam.zirmegghuette.zars.web.controller;
+
+import java.io.Serializable;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import net.soomsam.zirmegghuette.zars.persistence.dao.EntityNotFoundException;
+import net.soomsam.zirmegghuette.zars.service.UserService;
+import net.soomsam.zirmegghuette.zars.service.bean.UserBean;
+
+import org.apache.log4j.Logger;
+import org.springframework.context.annotation.Scope;
+
+import com.sun.faces.util.MessageFactory;
+
+@Named
+@Scope("request")
+public class ResetUserController implements Serializable {
+	private final static Logger logger = Logger.getLogger(ResetUserController.class);
+
+	@Inject
+	private transient UserService userService;
+
+	private boolean validNavigation = true;
+
+	private Long userId;
+
+	private String username;
+
+	private String password;
+
+	private String confirmPassword;
+
+	private UserBean savedUser;
+
+	public boolean isValidNavigation() {
+		return validNavigation;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(final Long userId) {
+		this.userId = userId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(final String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(final String password) {
+		this.password = password;
+	}
+
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+	public void setConfirmPassword(final String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+
+	public UserBean getSavedUser() {
+		return savedUser;
+	}
+
+	public void retrieveUser() {
+		if (null != this.userId) {
+			try {
+				final UserBean userBean = userService.retrieveUser(this.userId);
+				this.userId = userBean.getUserId();
+				this.username = userBean.getUsername();
+			} catch (final EntityNotFoundException entityNotFoundException) {
+				this.validNavigation = false;
+				final FacesMessage invalidUserIdFacesMessage = MessageFactory.getMessage("sectionsApplicationEditUserUserIdError", FacesMessage.SEVERITY_ERROR, null);
+				FacesContext.getCurrentInstance().addMessage(null, invalidUserIdFacesMessage);
+			}
+		}
+
+		if (!FacesContext.getCurrentInstance().isPostback() && (null == this.userId)) {
+			this.validNavigation = false;
+			final FacesMessage invalidUserIdFacesMessage = MessageFactory.getMessage("sectionsApplicationEditUserUserIdError", FacesMessage.SEVERITY_ERROR, null);
+			FacesContext.getCurrentInstance().addMessage(null, invalidUserIdFacesMessage);
+		}
+	}
+
+	public String reset() {
+		logger.debug("resetting user with id [" + userId + "]");
+		// TODO savedUser = userService.resetUser(userId, password, true);
+		return "resetUserConfirmation";
+	}
+}
