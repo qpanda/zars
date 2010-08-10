d1ce8673ff729f9d0910017356160a77cc6c2bcb
Erich Liebmann <7557807+qpanda@users.noreply.github.com>
2010-08-10 10:00:05 +0000

    migrated to primefaces 2.1 with new ui component library

--- src/main/webapp/net/soomsam/zirmegghuette/zars/web/controller/GroupReservationController.java
+++ src/main/webapp/net/soomsam/zirmegghuette/zars/web/controller/GroupReservationController.java
@@ -5,8 +5,8 @@ import java.util.Date;
 
 import javax.inject.Named;
 
+import org.primefaces.model.DefaultScheduleEvent;
 import org.primefaces.model.DefaultScheduleModel;
-import org.primefaces.model.ScheduleEventImpl;
 import org.primefaces.model.ScheduleModel;
 import org.springframework.context.annotation.Scope;
 
@@ -18,7 +18,7 @@ public class GroupReservationController implements Serializable {
 	public GroupReservationController() {
 		super();
 		this.schedule = new DefaultScheduleModel();
-		this.schedule.addEvent(new ScheduleEventImpl("test", new Date(), new Date()));
+		this.schedule.addEvent(new DefaultScheduleEvent("test", new Date(), new Date()));
 	}
 
 	public ScheduleModel getSchedule() {
