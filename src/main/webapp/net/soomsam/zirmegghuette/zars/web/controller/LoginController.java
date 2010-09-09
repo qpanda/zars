9c9e57e0f6176d63f6ce561daae1c74b0ac522ea
Erich Liebmann <7557807+qpanda@users.noreply.github.com>
2010-09-09 05:50:11 +0000

    started to enable security

--- src/main/webapp/net/soomsam/zirmegghuette/zars/web/controller/LoginController.java
+++ src/main/webapp/net/soomsam/zirmegghuette/zars/web/controller/LoginController.java
@@ -2,14 +2,27 @@ package net.soomsam.zirmegghuette.zars.web.controller;
 
 import java.io.Serializable;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 
 import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.sun.faces.util.MessageFactory;
 
 @Named
 @Scope("request")
 public class LoginController implements Serializable {
+	@Autowired
+	private transient AuthenticationManager authenticationManager;
+
 	@NotEmpty(message = "{sectionsWelcomeLoginUsernameError}")
 	private String username;
 
@@ -53,9 +66,17 @@ public class LoginController implements Serializable {
 	}
 
 	public String login() {
-		System.out.println("$$$ login");
-		this.loginFailed = true;
-		this.loginFailedMessage = "test";
-		return null;
+		try {
+			Authentication authenticationToken = new UsernamePasswordAuthenticationToken(getUsername(), getPassword());
+			Authentication authentication = authenticationManager.authenticate(authenticationToken);
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+		} catch (AuthenticationException authenticationException) {
+			// TODO error message
+			final FacesMessage invalidUserIdFacesMessage = MessageFactory.getMessage("sectionsApplicationUserUserIdError", FacesMessage.SEVERITY_ERROR, null);
+			FacesContext.getCurrentInstance().addMessage(null, invalidUserIdFacesMessage);
+			return null;
+		}
+
+		return "adminGroupReservation?faces-redirect=true";
 	}
 }
