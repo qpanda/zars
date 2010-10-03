650cfa24e9dc280706c17316f2b2446f63166b53
Erich Liebmann <7557807+qpanda@users.noreply.github.com>
2010-10-03 08:32:15 +0000

    started to implement view group reservation functionality

---
+++ src/main/webapp/net/soomsam/zirmegghuette/zars/web/controller/ViewGroupReservationController.java
@@ -0,0 +1,70 @@
+package net.soomsam.zirmegghuette.zars.web.controller;
+
+import java.io.Serializable;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import net.soomsam.zirmegghuette.zars.persistence.dao.EntityNotFoundException;
+import net.soomsam.zirmegghuette.zars.service.GroupReservationService;
+import net.soomsam.zirmegghuette.zars.service.bean.GroupReservationBean;
+
+import org.apache.log4j.Logger;
+import org.springframework.context.annotation.Scope;
+
+import com.sun.faces.util.MessageFactory;
+
+@Named
+@Scope("request")
+public class ViewGroupReservationController implements Serializable {
+	private final static Logger logger = Logger.getLogger(ViewGroupReservationController.class);
+
+	@Inject
+	protected transient GroupReservationService groupReservationService;
+
+	private boolean validNavigation = true;
+
+	private Long groupReservationId;
+
+	private GroupReservationBean savedGroupReservation;
+
+	public boolean isValidNavigation() {
+		return validNavigation;
+	}
+
+	public Long getGroupReservationId() {
+		return groupReservationId;
+	}
+
+	public void setGroupReservationId(final Long groupReservationId) {
+		this.groupReservationId = groupReservationId;
+	}
+
+	public GroupReservationBean getSavedGroupReservation() {
+		return savedGroupReservation;
+	}
+
+	public void setSavedGroupReservation(GroupReservationBean savedGroupReservation) {
+		this.savedGroupReservation = savedGroupReservation;
+	}
+
+	public void retrieveGroupReservation() {
+		if (null != this.groupReservationId) {
+			try {
+				savedGroupReservation = groupReservationService.retrieveGroupReservation(groupReservationId);
+			} catch (final EntityNotFoundException entityNotFoundException) {
+				this.validNavigation = false;
+				final FacesMessage invalidGroupReservationIdFacesMessage = MessageFactory.getMessage("sectionsApplicationGroupReservationGroupReservationIdError", FacesMessage.SEVERITY_ERROR, null);
+				FacesContext.getCurrentInstance().addMessage(null, invalidGroupReservationIdFacesMessage);
+			}
+		}
+
+		if (!FacesContext.getCurrentInstance().isPostback() && (null == this.groupReservationId)) {
+			this.validNavigation = false;
+			final FacesMessage invalidGroupReservationIdFacesMessage = MessageFactory.getMessage("sectionsApplicationGroupReservationGroupReservationIdError", FacesMessage.SEVERITY_ERROR, null);
+			FacesContext.getCurrentInstance().addMessage(null, invalidGroupReservationIdFacesMessage);
+		}
+	}
+}
