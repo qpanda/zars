32bf9c19ae0f5afa58727764fb272d3aa1536a01
Erich Liebmann <7557807+qpanda@users.noreply.github.com>
2010-11-07 09:45:29 +0000

    switched disable/enable user to single actionListener approach

--- src/main/webapp/net/soomsam/zirmegghuette/zars/web/controller/AdminUserController.java
+++ src/main/webapp/net/soomsam/zirmegghuette/zars/web/controller/AdminUserController.java
@@ -21,8 +21,6 @@ public class AdminUserController implements Serializable {
 
 	private final String commandLinkSelectedUserIdAttributeName = "selectedUserId";
 
-	private Long selectedUserId;
-
 	@Inject
 	private transient UserService userService;
 
@@ -30,35 +28,27 @@ public class AdminUserController implements Serializable {
 		return commandLinkSelectedUserIdAttributeName;
 	}
 
-	public Long getSelectedUserId() {
-		return selectedUserId;
-	}
-
-	public void setSelectedUserId(final Long selectedUserId) {
-		this.selectedUserId = selectedUserId;
-	}
-
 	public List<UserBean> getAllUsers() {
 		return userService.findAllUsers();
 	}
 
-	public void setSelectedUserId(final ActionEvent commandLinkActionEvent) {
+	public void onDisableUser(final ActionEvent commandLinkActionEvent) {
 		if ((null != commandLinkActionEvent) && (commandLinkActionEvent.getComponent() instanceof CommandLink)) {
-			final CommandLink commandLink = (CommandLink) commandLinkActionEvent.getComponent();
-			final Long commandLinkParameterValue = (Long) commandLink.getAttributes().get(commandLinkSelectedUserIdAttributeName);
-			setSelectedUserId(commandLinkParameterValue);
+			final CommandLink commandLink = (CommandLink)commandLinkActionEvent.getComponent();
+			final Long selectedUserId = (Long)commandLink.getAttributes().get(commandLinkSelectedUserIdAttributeName);
+
+			userService.disableUser(selectedUserId);
+			logger.debug("disabled user with id [" + selectedUserId + "]");
 		}
 	}
 
-	public String disableUser() {
-		userService.disableUser(selectedUserId);
-		logger.debug("disabled user with id [" + selectedUserId + "]");
-		return "adminUser?faces-redirect=true";
-	}
+	public void onEnableUser(final ActionEvent commandLinkActionEvent) {
+		if ((null != commandLinkActionEvent) && (commandLinkActionEvent.getComponent() instanceof CommandLink)) {
+			final CommandLink commandLink = (CommandLink)commandLinkActionEvent.getComponent();
+			final Long selectedUserId = (Long)commandLink.getAttributes().get(commandLinkSelectedUserIdAttributeName);
 
-	public String enableUser() {
-		userService.enableUser(selectedUserId);
-		logger.debug("enabled user with id [" + selectedUserId + "]");
-		return "adminUser?faces-redirect=true";
+			userService.enableUser(selectedUserId);
+			logger.debug("enabled user with id [" + selectedUserId + "]");
+		}
 	}
 }
