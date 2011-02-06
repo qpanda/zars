16696b0191e73fc52f814be83a26ca8bc12ffa86
Erich Liebmann <7557807+qpanda@users.noreply.github.com>
2011-02-06 07:39:28 +0000

    secured preference service

--- src/main/java/net/soomsam/zirmegghuette/zars/service/PreferenceService.java
+++ src/main/java/net/soomsam/zirmegghuette/zars/service/PreferenceService.java
@@ -1,16 +1,23 @@
 package net.soomsam.zirmegghuette.zars.service;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+
 import net.soomsam.zirmegghuette.zars.enums.PreferenceType;
 import net.soomsam.zirmegghuette.zars.service.bean.PreferenceBean;
 
 public interface PreferenceService {
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	public PreferenceBean createPreference(long userId, PreferenceType preferenceType, Object value);
 
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	public PreferenceBean findPreference(long userId, PreferenceType preferenceType);
 
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	public PreferenceBean updatePreference(long userId, PreferenceType preferenceType, Object value);
 
+	@PreAuthorize("isAuthenticated()")
 	public PreferenceBean findCurrentUserPreference(PreferenceType preferenceType);
 
+	@PreAuthorize("isAuthenticated()")
 	public PreferenceBean updateCurrentUserPreference(PreferenceType preferenceType, Object value);
 }
