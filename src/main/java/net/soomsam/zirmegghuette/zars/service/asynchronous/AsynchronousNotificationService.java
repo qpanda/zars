0a53987aa206291d06a98aabb3e0045fcd9ed3d9
Erich Liebmann <7557807+qpanda@users.noreply.github.com>
2011-03-21 08:42:02 +0000

    draft implementation of async notification service

--- src/main/java/net/soomsam/zirmegghuette/zars/service/asynchronous/AsynchronousNotificationService.java
+++ src/main/java/net/soomsam/zirmegghuette/zars/service/asynchronous/AsynchronousNotificationService.java
@@ -1,18 +1,23 @@
 package net.soomsam.zirmegghuette.zars.service.asynchronous;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
+import java.util.Set;
 import java.util.TimeZone;
 
 import net.soomsam.zirmegghuette.zars.enums.OperationType;
+import net.soomsam.zirmegghuette.zars.enums.ResourceBundleType;
+import net.soomsam.zirmegghuette.zars.enums.RoleType;
 import net.soomsam.zirmegghuette.zars.manager.MailManager;
 import net.soomsam.zirmegghuette.zars.manager.TemplateManager;
-import net.soomsam.zirmegghuette.zars.persistence.entity.User;
 import net.soomsam.zirmegghuette.zars.service.NotificationService;
 import net.soomsam.zirmegghuette.zars.service.PreferenceService;
+import net.soomsam.zirmegghuette.zars.service.UserService;
 import net.soomsam.zirmegghuette.zars.service.bean.BaseBean;
 import net.soomsam.zirmegghuette.zars.service.bean.GroupReservationBean;
+import net.soomsam.zirmegghuette.zars.service.bean.UserBean;
+import net.soomsam.zirmegghuette.zars.web.utils.MessageUtils;
 
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +30,9 @@ import freemarker.template.TemplateHashModel;
 @Service("notificationService")
 public class AsynchronousNotificationService implements NotificationService {
 	@Autowired
+	private UserService userService;
+
+	@Autowired
 	private PreferenceService preferenceService;
 
 	@Autowired
@@ -36,20 +44,34 @@ public class AsynchronousNotificationService implements NotificationService {
 	@Async
 	@Override
 	public void sendGroupReservationNotification(final OperationType operationType, final GroupReservationBean groupReservationBean) {
-		final List<User> notifyUserList = new ArrayList<User>(); // TODO finder, preference, and setting
-		sendNotification(operationType, groupReservationBean, notifyUserList, "groupReservationNotification", "NOTIFICATION_GROUPRESERVATION");
+		// TODO check setting to see if email sending is enabled
+
+		final UserBean booker = groupReservationBean.getBooker();
+		final UserBean beneficiary = groupReservationBean.getBeneficiary();
+		final UserBean accountant = groupReservationBean.getAccountant();
+		final List<UserBean> adminUserList = userService.findUsers(RoleType.ROLE_ADMIN);
+
+		final Set<UserBean> notifyUserBeanSet = new HashSet<UserBean>();
+		notifyUserBeanSet.add(booker);
+		notifyUserBeanSet.add(beneficiary);
+		notifyUserBeanSet.add(accountant);
+		notifyUserBeanSet.addAll(adminUserList);
+
+		sendNotification(operationType, groupReservationBean, notifyUserBeanSet, "groupReservationNotification", "NOTIFICATION_GROUPRESERVATION");
 	}
 
-	protected void sendNotification(final OperationType operationType, final BaseBean operationData, final List<User> notifyUserList, final String templateName, final String messageName) {
-		// TODO check email enabled setting here
-		for (final User notifyUser : notifyUserList) {
-			final Locale preferredLocale = preferenceService.determinePreferredLocale(notifyUser.getUserId());
-			final TimeZone preferredTimeZone = preferenceService.determinePreferredTimeZone(notifyUser.getUserId());
+	protected void sendNotification(final OperationType operationType, final BaseBean operationData, final Set<UserBean> notifyUserBeanSet, final String templateName, final String messageName) {
+		for (final UserBean notifyUserBean : notifyUserBeanSet) {
+			// TODO check notification preference
+			// TODO check if user is enabled, disabled users should not get notified
+			// TODO check if user has email address
+			final Locale preferredLocale = preferenceService.determinePreferredLocale(notifyUserBean.getUserId());
+			final TimeZone preferredTimeZone = preferenceService.determinePreferredTimeZone(notifyUserBean.getUserId());
 			final TemplateHashModel templateModel = createTemplateModel(operationType, operationData);
 
-			final String subject = "TODO"; // TODO localized enum lookup
+			final String subject = MessageUtils.obtainMessage(ResourceBundleType.DISPLAY_MESSAGES, messageName, preferredLocale);
 			final String message = templateManager.generateDocument(templateName, templateModel, preferredLocale, preferredTimeZone);
-			mailManager.sendMail(notifyUser.getEmailAddress(), subject, message);
+			mailManager.sendMail(notifyUserBean.getEmailAddress(), subject, message);
 		}
 	}
 
